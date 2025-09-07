c71256fc764c1929502d2e179e74d72d41d08047
vladimir-cpu <125793498+vladimir-cpu@users.noreply.github.com>
2025-09-07 18:48:57 +0300

    Update order.js

--- netlify/functions/order.js
+++ netlify/functions/order.js
@@ -19,23 +19,24 @@ exports.handler = async function(event, context) {
       statusCode: 405,
       headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: 'Только POST запросы' })
-    };
+    }; 
   }
 
   try {
     const { name, phone, address, items, total, user_id } = JSON.parse(event.body);
-
-    const BOT_TOKEN = '8467789771:AAHa7YBRUXSzz3P3pOViRINIS1Lzwg3YJ2c';
+    const BOT_TOKEN = process.env.BOT_TOKEN; 
     const CHAT_ID = user_id;
 
-    let text = `📦 *Новый заказ!* \n\n`;
-    text += `👤 *Имя:* ${name}\n`;
-    text += `📞 *Телефон:* ${phone}\n`;
-    text += `🏠 *Адрес:* ${address}\n\n`;
-    text += `📋 *Блюда:*\n`;
+    let text = `*НОВЫЙ заказ!* \n\n`;
+    text += `*Имя:* ${name}\n`;
+    text += `*Телефон:* ${phone}\n`;
+    text += `*Адрес:* ${address}\n\n`;
+    text += `*Блюда:*\n`;
+    
     items.forEach(item => {
-      text += `— ${item.name} (${item.price}₽)\n`;
+      text += `${item.name} (${item.price}₽)\n`;
     });
+    
     text += `\n💰 *Итого:* ${total}₽`;
 
     const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`; 
@@ -45,7 +46,7 @@ exports.handler = async function(event, context) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         chat_id: CHAT_ID,
-        text,
+        text: text,
         parse_mode: 'Markdown'
       })
     });
